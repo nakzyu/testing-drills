03ca7b38e1966a74e99cacca671c01b204621e1a
nakzyu <nakzyu@gmail.com>
2022-01-19 21:14:36 +0900

    Section 4: Init

--- sundaes-on-demand/src/App.test.js
+++ sundaes-on-demand/src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import userEvent from "@testing-library/user-event";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(linkElement).not.toBeInTheDocument();
 });
